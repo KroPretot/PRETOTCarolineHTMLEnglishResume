a325485d3da93dc0f3f7de0bd941f34a86f0357a
KroPretot <32820405+KroPretot@users.noreply.github.com>
2018-01-22 09:03:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$('div.JavascriptTest1').click(function() {
+       $(this).animate({
+           height: '+=10px'
+       });
+	});
+	//click peut être remplacé par (entre autre) : mouseenter et mouseleave
+	//height peut être remplacé par (entre autre) : width
+	$('div.JavascriptTest2').mouseenter(function() {
+		//Toggle permet de basculer un élément d'un état à un autre. ex : si l'élément est visible, il devient invisible.
+		$(this).toggle(1000);
+		});
+	$('div.JavascriptTest2bis').mouseenter(function() {
+		$(this).fadeOut(1000);
+		});
+
+
+
+
+
+		
+});
